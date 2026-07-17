4fe1cc70381c77285e9189444c6679c0db7e95e7
Mazen Elsakka <mazenelsakka@MacBook-Pro.local>
2026-07-17 23:33:11 +0300

    Scale image, add red circle background, revert to 2 columns for moving name

--- src/components/VerticalName.jsx
+++ src/components/VerticalName.jsx
@@ -13,13 +13,17 @@ export default function VerticalName() {
       <div className="marquee-column">
         <div className="marquee-content-up">
           <span className="name-word">{name1}</span>
-          <span className="name-word name-word-alt">{name2}</span>
-          {/* Duplicate for seamless loop */}
           <span className="name-word" aria-hidden="true">{name1}</span>
-          <span className="name-word name-word-alt" aria-hidden="true">{name2}</span>
+        </div>
+      </div>
+      <div className="marquee-column">
+        <div className="marquee-content-down">
+          <span className="name-word">{name2}</span>
+          <span className="name-word" aria-hidden="true">{name2}</span>
         </div>
       </div>
     </div>
   );
 }
+
 
